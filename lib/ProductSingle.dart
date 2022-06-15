457af851ed67810939a9120e7a8eab4ba61c8119
Rawan M Al-Shurbaji <rawnsh2218@gmail.com>
2022-06-15 17:38:05 +0300

    first

--- lib/ProductSingle.dart
+++ lib/ProductSingle.dart
@@ -4,16 +4,14 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'helpers/webservice_manager.dart';
 import 'models/product.dart';
 
-class ProductSingle extends StatelessWidget{
-
-  var id ;
+class ProductSingle extends StatelessWidget {
+  var id;
   ProductSingle(this.id);
 
   @override
   Widget build(BuildContext context) {
     print(id);
     return Scaffold(
-
         appBar: AppBar(
           backgroundColor: Color(0xFF769E49),
           leading: IconButton(
@@ -23,61 +21,56 @@ class ProductSingle extends StatelessWidget{
           title: Text("Details"),
           centerTitle: false,
         ),
-
-        body: ListView(
-          children:[
-            Container(
+        body: ListView(children: [
+          Container(
             margin: const EdgeInsets.only(left: 20),
             child: FutureBuilder<Product>(
               future: Service().getProductDetails(id),
-              builder: (context,snapshot)  {
-                if(snapshot.hasData){
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
                   print("id");
-                  return  Column(
+                  return Column(
                     children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 30, right: 40),
-                    child: Column(
-                  crossAxisAlignment:
-                  CrossAxisAlignment.start,
-                    children: [
-                      Stack(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(
-                                20, 16, 0, 0),
-                            child: Image.network(
-                              snapshot.data!.image!,
-                              fit: BoxFit.fitWidth,
-                              alignment: Alignment.bottomLeft,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 25, left: 290),
-                            child: Container(
-                              width: 30,
-                              height: 30,
-                              child: FavoriteButton(
-                                iconSize: 40,
-                                isFavorite: false,
-                                valueChanged: (_isFavorite) {
-                                  print(
-                                      'Is Favorite : $_isFavorite');
-                                },
+                      Container(
+                        margin: const EdgeInsets.only(top: 30, right: 40),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Stack(
+                                children: [
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(20, 16, 0, 0),
+                                    child: Image.network(
+                                      snapshot.data!.image!,
+                                      fit: BoxFit.fitWidth,
+                                      alignment: Alignment.bottomLeft,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 25, left: 290),
+                                    child: Container(
+                                      width: 30,
+                                      height: 30,
+                                      child: FavoriteButton(
+                                        iconSize: 40,
+                                        isFavorite: false,
+                                        valueChanged: (_isFavorite) {
+                                          print('Is Favorite : $_isFavorite');
+                                        },
+                                      ),
+                                      decoration: const BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(50)),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.all(
-                                    Radius.circular(50)),
-                              ),
-                            ),
-                          ),
-                        ],
+                            ]),
                       ),
-                ]
-                  ),
-                ),
-
                       Align(
                           alignment: Alignment.bottomLeft,
                           child: Column(
@@ -85,42 +78,33 @@ class ProductSingle extends StatelessWidget{
                               Align(
                                   alignment: Alignment.bottomLeft,
                                   child: Container(
-                                    margin: const EdgeInsets.only(bottom: 5, top: 15),
-                                    child:  Text(
+                                    margin: const EdgeInsets.only(
+                                        bottom: 5, top: 15),
+                                    child: Text(
                                       snapshot.data!.title!,
                                       style: TextStyle(fontSize: 18),
                                     ),
-                                  )
-                              ),
-
+                                  )),
                               Align(
-                                  alignment: Alignment.bottomLeft,
-                                  child: Container(
-                                    margin: const EdgeInsets.only(bottom: 5),
-                                    child: RatingBarIndicator(
-                                      rating: snapshot
-                                          .data!
-                                          .rating!
-                                          .rate!,
-                                      itemBuilder: (context,
-                                          index) =>
-                                      const Icon(Icons.star,
-                                          color:
-                                          Colors.amber),
-                                      itemCount: 5,
-                                      itemSize: 18.0,
-                                      direction: Axis.horizontal,
-                                    ),
+                                alignment: Alignment.bottomLeft,
+                                child: Container(
+                                  margin: const EdgeInsets.only(bottom: 5),
+                                  child: RatingBarIndicator(
+                                    rating: snapshot.data!.rating!.rate!,
+                                    itemBuilder: (context, index) => const Icon(
+                                        Icons.star,
+                                        color: Colors.amber),
+                                    itemCount: 5,
+                                    itemSize: 18.0,
+                                    direction: Axis.horizontal,
                                   ),
+                                ),
                               ),
-
-
                               Align(
                                 alignment: Alignment.bottomLeft,
                                 child: Container(
                                   margin: const EdgeInsets.only(top: 5),
-                                  child:
-                                  Row(
+                                  child: Row(
                                     children: const [
                                       Icon(
                                         Icons.circle,
@@ -140,15 +124,17 @@ class ProductSingle extends StatelessWidget{
                               ),
                               Container(
                                 // margin: const EdgeInsets.only(top: 20),
-                                margin: const EdgeInsets.only(left: 5, top: 10, right: 5),
+                                margin: const EdgeInsets.only(
+                                    left: 5, top: 10, right: 5),
                                 width: 350,
-                                child:  Text(
+                                child: Text(
                                   snapshot.data!.description!,
                                   style: TextStyle(fontSize: 16),
                                 ),
                               ),
                               Container(
-                                margin: const EdgeInsets.only(top: 20,bottom: 20),
+                                margin:
+                                    const EdgeInsets.only(top: 20, bottom: 20),
                                 child: Row(
                                   children: [
                                     const Icon(
@@ -163,14 +149,13 @@ class ProductSingle extends StatelessWidget{
                                     Container(
                                       margin: const EdgeInsets.only(right: 20),
                                       child: ElevatedButton(
-                                        onPressed: () {
-                                        },
+                                        onPressed: () {},
                                         child: const Center(
                                           child: Text('Buy Now'),
                                         ),
                                         style: ElevatedButton.styleFrom(
-                                          primary:
-                                          const Color.fromRGBO(105, 160, 58, 1),
+                                          primary: const Color.fromRGBO(
+                                              105, 160, 58, 1),
                                           padding: const EdgeInsets.symmetric(
                                               horizontal: 50, vertical: 15),
                                         ),
@@ -183,19 +168,12 @@ class ProductSingle extends StatelessWidget{
                           ))
                     ],
                   );
-                }
-                else{
-
+                } else {
                   return const Center(child: CircularProgressIndicator());
-
                 }
               },
-
             ),
           ),
-    ]
-        )
-    );
+        ]));
   }
-
 }
